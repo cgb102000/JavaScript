c99e1f07c5a720bda1d3905c6ef2d5e20552812f
cgb102000 <>
2022-08-20 14:27:52 -0300

    Teste

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# JavaScript
-Aqui ficarão todos meu projetos em JS.
+# html-css
+ HTML5 e CSS3 e JS
+ Agora vou hospedar meus arquivos no git e github.
+ 
+<a href="https://cgb102000.github.io/javascript/teste">Para acessar meu site clique no link. </a>
---
+++ script5.js
@@ -0,0 +1,26 @@
+// JavaScript demonstration
+function clique(button) { /*Aqui o button é opcional */
+  var square = document.getElementById("square");  /*nome da variavel = document.getElementById("Nome da variavel") */ /*Aqui fazemos a ligaçao para o id HTML */
+  square.style.backgroundColor = "greenyellow";
+  square.style.backgroundImage = "url('botao.gif')"
+  document.square.style.backgroundImage = "url('botao.gif')"
+
+  
+}
+function clique2(button) { /*Aqui o button é opcional */
+  var squar2e = document.getElementById("square2");  /*nome da variavel = document.getElementById("Nome da variavel") */ /*Aqui fazemos a ligaçao para o id HTML */
+  square2.style.backgroundColor = "green";
+  square2.style.backgroundImage = "url('botao2.gif')"
+  document.square2.style.backgroundImage = "url('botao2.gif')"
+}
+function clique3(button) { /*Aqui o button é opcional */
+  var square3 = document.getElementById("square3");  /*nome da variavel = document.getElementById("Nome da variavel") */ /*Aqui fazemos a ligaçao para o id HTML */
+  square3.style.backgroundColor = "green";
+  square3.style.backgroundImage = "url('botao3.gif')"
+  document.square3.style.backgroundImage = "url('botao3.gif')"
+}
+
+
+
+
+
