3527a81d07b7223addc49cce8d6c54816859f753
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2022-10-27 06:48:00 -0300

    Atualizando

---
+++ Aulas/ex001/script.js
@@ -0,0 +1,3 @@
+window.alert("Meu primeiro código em JS...") /*Comando utilizado para criar uma janela com texto */
+window.confirm("Está aprendendo?") /*Comando utilizado para criar uma janela de confirmação */
+window.prompt("Qual é seu nome?") /*Comando utilizado para criar uma janela que o usúario pode digitar */
